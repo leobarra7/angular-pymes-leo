5ba54967bc49284c19297aa152592f83aeb328a2
leobarra7 <leoobarragan@gmail.com>
2020-05-13 16:19:30 -0300

    1 file modified, 1 added

--- src/app/components/articulos-familias/articulos-familias.component.ts
+++ src/app/components/articulos-familias/articulos-familias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticuloFamilia, ArticulosFamilias } from 'src/app/models/articulo-familia';
 
 @Component({
   selector: 'app-articulos-familias',
