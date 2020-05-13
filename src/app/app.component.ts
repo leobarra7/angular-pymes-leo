eaeda2416804d4f0b53e514dc563598ab2265f60
leobarra7 <leoobarragan@gmail.com>
2020-05-13 15:35:08 -0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  titulo = 'Pymes Demo - 2020';
   name = 'Angular ' + VERSION.major;
 }
